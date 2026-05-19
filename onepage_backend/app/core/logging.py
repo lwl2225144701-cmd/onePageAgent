48528e2d0a66a9a1336693d80de11bc39f2ae15c
78 <2225144701@qq.com>
2026-05-19 21:26:50 +0800

    fix(backend): stabilize structlog stdlib logging

---
+++ onepage_backend/app/core/logging.py
@@ -0,0 +1,27 @@
+import structlog
+import logging
+
+
+def setup_logging(debug: bool = False):
+    level = "DEBUG" if debug else "INFO"
+    logging.basicConfig(
+        level=level,
+        format="%(message)s",
+    )
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.dev.ConsoleRenderer() if debug else structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
